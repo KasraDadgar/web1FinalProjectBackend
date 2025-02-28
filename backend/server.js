556f07d589240cf9339165c0ba5e74cd16112b18
unknown <1212dadgar@gmail.com>
2025-02-28 11:33:12 +0330

    third commit

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,6 @@ const { neon } = require('@neondatabase/serverless');
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -184,6 +183,6 @@ app.delete("/api/admin/menu/:id", verifyToken, verifyAdmin, async (req, res) => 
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
