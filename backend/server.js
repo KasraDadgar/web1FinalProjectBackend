9393b8b9dceb96f5e7d8f2660dacd3fccf8b2e43
unknown <1212dadgar@gmail.com>
2025-02-28 12:07:00 +0330

    deployment commit

--- backend/server.js
+++ backend/server.js
@@ -183,6 +183,7 @@ app.delete("/api/admin/menu/:id", verifyToken, verifyAdmin, async (req, res) => 
 });
 
 // Start Server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
